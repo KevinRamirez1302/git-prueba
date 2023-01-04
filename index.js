21d7ac12e0d3e39942aa288aa1821d86421fb2dc
Kevin <kevin.alexanderra1302@gmail.com>
2023-01-04 15:18:29 -0400

    Encriptador

---
+++ index.js
@@ -0,0 +1,12 @@
+const encriptar = document.getElementById("botonUno");
+const desencriptar = document.getElementById("boton2");
+const contenidoEliminar = document.getElementById("eliminar");
+const textoIngresado = []
+
+
+encriptar.addEventListener("click", function(){
+
+    contenidoEliminar.classList.add("quitar");
+});
+
+
